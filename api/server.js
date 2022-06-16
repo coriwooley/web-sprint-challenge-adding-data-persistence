09c5d8e8b94d2f7231afeac919444261778fa855
Cori Wooley <wooleycori@gmail.com>
2022-06-16 08:38:53 -0800

    wired server/index.js, connected routes

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const projectRoutes = require('./project/router');
+const resourceRoutes = require('./resource/router');
+const taskRoutes = require('./task/router');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/project', projectRoutes);
+server.use('/api/resource', resourceRoutes);
+server.use('/api/task', taskRoutes)
+
+
+module.exports = server;
