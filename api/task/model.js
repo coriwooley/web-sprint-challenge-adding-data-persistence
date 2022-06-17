fd7f66778987b02407aefd6f97a07f3bcb573cd3
Cori Wooley <wooleycori@gmail.com>
2022-06-17 06:49:31 -0800

    fixed tasks find() model function. mvp

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,8 @@
 const db = require('../../data/dbConfig')
 
 function find(){
-    return db('tasks')
+    return db('tasks as t').leftJoin('projects as p', 'p.project_id', 't.project_id')
+    .select('task_id', 'task_description', 'task_notes', 'task_completed', 'p.project_name', 'p.project_description')
 }
 
 function insert(task){
